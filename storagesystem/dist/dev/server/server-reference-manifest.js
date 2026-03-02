74fec7332165567b2f84a2b0609a0ec8c6e4c8ab
Ahmad Alshomaree <220208706@ostimteknik.edu.tr>
2026-03-02 12:28:20 +0300

    May 2 version 2

--- storagesystem/dist/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"0Ho3gxyEZ7iALW9Zw7OlXssTTo9Tsvcr2/CaBRkqXM4=\"\n}"
--- storagesystem/dist/dev/static/chunks/605a9_next_dist_client_components_builtin_global-error_14bdcfdf.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/605a9_next_dist_6796b5ec._.js"
-],
-    source: "dynamic"
-});
