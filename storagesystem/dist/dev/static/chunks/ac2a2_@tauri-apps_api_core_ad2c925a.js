a237babcd715cd7675090ead9418b3ff2face232
Ahmad Alshomaree <220208706@ostimteknik.edu.tr>
2026-08-07 16:31:44 +0300

    Aug 7 version 3

--- storagesystem/dist/dev/static/chunks/ac2a2_@tauri-apps_api_core_ad2c925a.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/ac2a2_@tauri-apps_api_c6fb2436._.js"
-],
-    source: "dynamic"
-});
